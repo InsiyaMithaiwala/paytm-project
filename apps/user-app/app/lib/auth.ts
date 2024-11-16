18b66c75b011f90e899c350d712143588427c89b
InsiyaMithaiwala <mithaiwalainsiya7@gmail.com>
2024-11-16 12:14:26 +0530

    added

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -1,3 +1,4 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
 import db from "@repo/db/client";
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcrypt";
@@ -53,6 +54,7 @@ export const authOptions = {
           },
         })
     ],
+    
     secret: process.env.JWT_SECRET || "secret",
     callbacks: {
         // TODO: can u fix the type here? Using any is bad
